e454daf8b5c92dd75a2da5111ad2d773fa48df9b
tomer doron <tomer.doron@gmail.com>
2011-01-05 12:37:36 -0800

    core stanzas

--- src/main/scala/org/xmpp/protocol/Roster.scala
+++ src/main/scala/org/xmpp/protocol/Roster.scala
@@ -5,9 +5,95 @@ package org.xmpp
 		import scala.collection._
 		import scala.xml._
 		
-		class Roster(xml:Node) extends IQ(xml)
+		import org.xmpp.protocol.Protocol._
+		
+		final object Roster
+		{
+			
+		}
+		
+		final class Roster(xml:Node) extends IQ(xml)
 		{
 		
 		}
+		
+		
+		final object RosterItem
+		{
+			val TAG = "item"
+			
+			def apply(jid:Option[JID], name:Option[String], subscrption:Option[RosterItemSubscription.Value], ask:Option[RosterItemAsk.Value], groups:Option[Seq[String]]):RosterItem =
+			{
+				val children = mutable.ListBuffer[Node]()
+				if (!groups.isEmpty) groups.get.foreach( group => children += <groups>{ group }</groups> )
+				var metadata:MetaData = Null
+				if (!jid.isEmpty) metadata = metadata.append(new UnprefixedAttribute("jid", Text(jid.get), Null))
+				if (!name.isEmpty) metadata = metadata.append(new UnprefixedAttribute("name", Text(name.get), Null))
+				if (!subscrption.isEmpty) metadata = metadata.append(new UnprefixedAttribute("subscrption", Text(subscrption.get.toString), Null))
+				if (!ask.isEmpty) metadata = metadata.append(new UnprefixedAttribute("ask", Text(ask.get.toString), Null))
+				return new RosterItem(Elem(null, TAG, metadata, TopScope, children:_*))
+			}
+		}
+		
+		final class RosterItem(xml:Node) extends XmlWrapper(xml)
+		{
+			parse
+			
+			// getters
+			private var _jid:Option[JID] = None
+			private def jid:Option[JID] = _jid
+			
+			private var _name:Option[String] = None
+			private def name:Option[String] = _name
+			
+			private var _subscrption:Option[RosterItemSubscription.Value] = None
+			private def subscrption:Option[RosterItemSubscription.Value] = _subscrption
+			
+			private var _ask:Option[RosterItemAsk.Value] = None
+			private def ask:Option[RosterItemAsk.Value] = _ask			
+			
+			private var _groups:Option[Seq[String]] = None
+			private def groups:Option[Seq[String]] = _groups
+			
+			private def parse
+			{
+				val jid = (this.xml \ "@jid").text
+				_jid = if (jid.isEmpty) None else Some(JID(jid))
+				
+				val name = (this.xml \ "@name").text
+				_name = if (name.isEmpty) None else Some(name)
+				
+				val subscrption = (this.xml \ "@subscrption").text
+				_subscrption = if (subscrption.isEmpty) None else Some(RosterItemSubscription.withName(subscrption))
+				
+				val ask = (this.xml \ "@ask").text
+				_ask = if (ask.isEmpty) None else Some(RosterItemAsk.withName(ask))
+				
+				val groupNodes = (this.xml \ "group")
+				_groups = if (0 == groupNodes.length) None else Some( groupNodes.map( node => node.label ) )
+			}			
+		}
+		
+		
+		final object RosterItemSubscription extends Enumeration
+		{
+			type value = Value
+			
+			val Unknown = Value("unknown") // internal use
+			val None = Value("none")
+			val To = Value("to")
+			val From = Value("from")
+			val Both = Value("both")
+			val Remove = Value("remove")			
+		}
+		
+		final object RosterItemAsk extends Enumeration
+		{
+			type value = Value
+			
+			val Unknown = Value("unknown") // internal use
+			val Subscribe = Value("subscribe")
+			val Unsubscribe = Value("unsubscribe")
+		}		
 	}
 }
