b24250884f9850552644753ce5b1aaceacf4f44b
tomer doron <tomer.doron@gmail.com>
2011-01-08 21:12:19 -0800

    core stanzas

---
+++ src/main/scala/org/xmpp/protocol/roster/RosterGet.scala
@@ -0,0 +1,35 @@
+package org.xmpp
+{
+	package protocol.roster
+	{
+		import scala.collection._
+		import scala.xml._
+		
+		import org.xmpp.protocol._
+		import org.xmpp.protocol.iq._
+		
+		import org.xmpp.protocol.Protocol._
+		
+		object RosterGet extends ExtendedStanzaBuilder[RosterGet]
+		{			
+			val kind = Get.kindName
+			val name = Query.name
+			val namespace = "jabber:iq:roster"
+			
+			def apply(id:Option[String], to:Option[JID], from:Option[JID]):RosterGet = 
+			{
+				val extension = Query(namespace)
+				val xml = Get.build(id, to, from, Some(List(extension)))
+				return apply(xml)
+			}
+			
+			def apply(xml:Node):RosterGet = new RosterGet(xml)
+		}
+		
+		class RosterGet(xml:Node) extends Get(xml)
+		{
+			def result(items:Seq[Item]):RosterResult = RosterResult(this.id, this.from, this.to, items)
+		}
+		
+	}
+}
