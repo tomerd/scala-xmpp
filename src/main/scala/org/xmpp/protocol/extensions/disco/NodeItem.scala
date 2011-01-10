1c0dbd9bb184f1f755dd7bdb898d26c982fc245e
tomer doron <tomer.doron@gmail.com>
2011-01-09 18:55:46 -0800

    extensions support

---
+++ src/main/scala/org/xmpp/protocol/extensions/disco/NodeItem.scala
@@ -0,0 +1,42 @@
+package org.xmpp
+{
+	package protocol.extensions.disco
+	{
+		import scala.collection._
+		import scala.xml._
+		
+		import org.xmpp.protocol._
+		
+		import org.xmpp.protocol.Protocol._
+		
+		object NodeItem
+		{			
+			def apply(jid:JID, node:String, name:Option[String]=None):Node = apply(build(jid, node, name))
+			
+			def apply(xml:Node) = new NodeItem(xml)
+			
+			def build(jid:JID, node:String, name:Option[String]):Node =
+			{
+				var metadata:MetaData = new UnprefixedAttribute("jid", Text(jid.get), Null)
+				metadata = metadata.append(new UnprefixedAttribute("node", Text(node.get), Null))
+				if (!name.isEmpty) metadata = metadata.append(new UnprefixedAttribute("name", Text(name.get), Null))
+				return Item.build(metadata)
+			}
+		}
+		
+		class NodeItem(xml:Node) extends Item(xml)
+		{			
+			// getters			
+			val jid:JID = (this.xml \ "@jid").text
+
+			val node:String = (this.xml \ "@node").text
+			
+			val name:Option[String] = 
+			{
+				val name = (this.xml \ "@name").text
+				if (name.isEmpty) None else Some(name)
+			}
+		}
+				
+	}
+}
