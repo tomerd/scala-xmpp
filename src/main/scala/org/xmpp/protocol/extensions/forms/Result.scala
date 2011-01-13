e39f044ed14fa3f1dedc93537f1ab101c1640c6a
tomer doron <tomer.doron@gmail.com>
2011-01-12 18:05:01 -0800

    extensions support

--- src/main/scala/org/xmpp/protocol/extensions/forms/Result.scala
+++ src/main/scala/org/xmpp/protocol/extensions/forms/Result.scala
@@ -20,6 +20,12 @@ package org.xmpp
 			def apply(id:Option[String], to:Option[JID], from:Option[JID], title:Option[String], fields:Seq[Field]):Result = apply(id, to, from, title, None, fields)
 			
 			def apply(id:Option[String], to:Option[JID], from:Option[JID], title:Option[String], instructions:Option[Seq[String]], fields:Seq[Field]):Result = apply(build(id, to, from, title, instructions, fields))
+			
+			def apply(id:Option[String], to:Option[JID], from:Option[JID], reported:ResultHeader, items:Seq[ResultItem]):Result =  apply(build(id, to, from, None, None, reported, items))
+			
+			def apply(id:Option[String], to:Option[JID], from:Option[JID], title:Option[String], reported:ResultHeader, items:Seq[ResultItem]):Result =  apply(build(id, to, from, title, None, reported, items))
+			
+			def apply(id:Option[String], to:Option[JID], from:Option[JID], title:Option[String], instructions:Option[Seq[String]], reported:ResultHeader, items:Seq[ResultItem]):Result =  apply(build(id, to, from, title, instructions, reported, items))
  			
 			def apply(xml:Node):Result = new Result(xml)
 			
@@ -27,6 +33,14 @@ package org.xmpp
 			{
 				Form.build(id, to, from, Result.formType, title, instructions, fields)
 			}
+			
+			def build(id:Option[String], to:Option[JID], from:Option[JID], title:Option[String], instructions:Option[Seq[String]], reported:ResultHeader, items:Seq[ResultItem]):Node =
+			{
+				val children = mutable.ListBuffer[Node]()
+				children += reported
+				children ++= items
+				Form.build(id, to, from, Result.formType, title, instructions, children)
+			}			
 		}
 		
 		class Result(xml:Node) extends Form(xml, Result.formType)
