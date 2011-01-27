f586149daeb1f93de4dad1e695197ed13a287cea
tomer doron <tomer.doron@gmail.com>
2011-01-26 19:34:17 -0800

    naggati implementation

--- src/main/scala/org/xmpp/codec/Codec.scala
+++ src/main/scala/org/xmpp/codec/Codec.scala
@@ -1,13 +1,16 @@
 package org.xmpp
 {
 	package codec
-	{
+	{		
+		import scala.collection._
 		import scala.xml._
+		import scala.xml.pull._
 		
 		import org.apache.mina.core.buffer.IoBuffer
 		import org.apache.mina.core.session.IdleStatus
 		import org.apache.mina.core.session.IoSession
 		import org.apache.mina.filter.codec._
+	
 		import net.lag.naggati._
 		import net.lag.naggati.Steps._
 		
@@ -17,7 +20,7 @@ package org.xmpp
 		object Codec
 		{
 			val encoder = new XmppEncoder
-			val decoder = new XmppDecoder
+			val decoder = new XmppDecoder			
 		}
 		
 		class XmppEncoder extends ProtocolEncoder
@@ -34,13 +37,9 @@ package org.xmpp
 			}
 		}
 		
-		// FIXME: this currently assumes 1 packaet = 1 stanza, 
-		// but this is a wrong assumption, need to buffer the content until start tag and end tag match
 		class XmppDecoder extends Decoder(readAll
 		{ buffer =>
 			{
-				//state.out.write(string) 
-				//End
 				val string = new String(buffer, "UTF-8")
 				
 				if (StreamHead.qualifies(string))
@@ -57,35 +56,100 @@ package org.xmpp
   				{
   					try
   					{
-  						val xml = XML.loadString(string)
-						val message = xml.label match
-						{
-							case StreamError.tag => StreamError(xml)
-							case Handshake.tag => Handshake(xml)
-							case _ => Stanza(xml)
+						var level = 0
+						var children = new mutable.HashMap[Int, mutable.ListBuffer[Node]]
+						var attributes = new mutable.HashMap[Int, MetaData]
+						var scope = new mutable.HashMap[Int, NamespaceBinding]
+						
+						var result:Option[Tuple2[AnyRef, Option[Iterator[XMLEvent]]]] = None
+						val tokenizer = new XMLEventReader(scala.io.Source.fromString(string))
+						
+						while (tokenizer.hasNext && result.isEmpty) 
+						{			
+							tokenizer.next match
+							{
+								case tag:EvText => children(level) += new Text(tag.text)
+								case tag:EvProcInstr => children(level) += new ProcInstr(tag.target, tag.text)
+								case tag:EvComment => children(level) += new Comment(tag.text)
+								case tag:EvEntityRef => children(level) += new EntityRef(tag.entity)
+								case tag:EvElemStart => 
+								{			
+									level = level+1
+									if (!attributes.contains(level)) attributes += level -> tag.attrs else attributes(level) = tag.attrs
+									if (!scope.contains(level)) scope += level -> tag.scope else scope(level) = tag.scope
+									if (!children.contains(level)) children += level -> new mutable.ListBuffer[Node]() else children(level) = new mutable.ListBuffer[Node]()
+								}
+								case tag:EvElemEnd => 
+								{									
+									val node = Elem(tag.pre, tag.label, attributes(level), scope(level), children(level):_*)
+									
+									level = level-1								
+									if (0 == level)
+									{
+										// deal with leftover
+										val leftover = tokenizer.hasNext match
+										{
+											case true => Some(tokenizer.slice(tokenizer.indexOf(tag), tokenizer.length))
+											case false => None
+										}
+										
+										// parsed message
+										val message = node.label match
+										{
+											case StreamError.tag => StreamError(node)
+											case Handshake.tag => Handshake(node)
+											case _ => Stanza(node)
+										}
+										
+										result = Some(Tuple2(message, leftover))
+									}
+									else
+									{
+										children(level) += node		
+									}				
+								}
+							}
 						}
-						state.out.write(message) 
-						End
-  					}
-  					catch
-  					{
-  						case pe:org.xml.sax.SAXParseException =>
-  						{
-  							// TODO: implement this
-  							// parial xml recieved, wait for rest of it in the next packet
-  							println("partial xml recieved " + string)
-  							End
-  						}
-  						case e:Exception =>
-  						{
-  							// TODO: do something more intelligent here
-  							println("internal error decoding packet " + string)
-  							End
-  						}
-  						
-  					}
-  				}
+						
+						result match
+						{
+  							case Some(tuple) =>
+  							{
+  								// deal with leftover
+  								if (!tuple._2.isEmpty)
+  								{
+	  								// FIXME: implement this
+	  								println("tail found")
+	  								val save = tuple._2.get
+  								}
+  								
+  								// write message out
+  								state.out.write(tuple._1)
+  								End
+  							}
+  							case None =>
+  							{
+  								// deal with parial
+  								// FIXME: implement this
+  								println("partial found")
+  								val save = tokenizer
+  								End
+  							}
+						}
+						
+					}
+					catch
+					{
+						case e:Exception =>
+						{
+							// TODO: do something more intelligent here
+							println("internal error decoding packet " + string + "\nerror: " + e)
+							End
+						}
+					}
+				}
 			}
 		})
+		
 	}
 }
