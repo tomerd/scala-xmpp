df4ee6f4eda8d8b6c35ee5f9e386b1f398e1d450
tomer doron <tomer.doron@gmail.com>
2011-02-17 12:28:28 -0800

    rpc extension

--- src/main/scala/org/simbit/xmpp/protocol/extensions/rpc/Parameter.scala
+++ src/main/scala/org/simbit/xmpp/protocol/extensions/rpc/Parameter.scala
@@ -3,7 +3,6 @@ package org.simbit.xmpp
 	package protocol.extensions.rpc
 	{		
 		import scala.xml._
-		import scala.collection._
 		
 		import org.simbit.xmpp.protocol._
 				
@@ -11,35 +10,117 @@ package org.simbit.xmpp
 		{
 			val tag = "param"
 			
-			// stupid, but required when coming from java
-			def apply(value:java.lang.Object):Parameter = 
+			def apply(value:Object):Parameter = apply(buildFullNode(buildValueNode(value)))
+			
+			private def buildValueNode(value:Object):Node = 
 			{
-				value match
+				castToSupportedType(value) match
 				{
-					case value:java.lang.String => apply("string", value)
-					case value:java.lang.Integer => apply("int", value.toString)
-					case value:java.lang.Double => apply("double", value.toString)
-					case value:java.lang.Boolean => apply("boolean", value.toString)
-					// TODO, finish these (float, date, array, map, etc)
+					case null => buildDataTypeNode("nil", "")
+					case value:java.lang.String => buildDataTypeNode("string", value)
+					case value:java.lang.Integer => buildDataTypeNode("int", value.toString)
+					case value:java.lang.Double => buildDataTypeNode("double", value.toString)
+					case value:java.lang.Boolean => buildDataTypeNode("boolean", value.toString)
+					case value:java.util.Date => buildDataTypeNode("dateTime.iso8601", dateToIso8601(value))
+					case array:Array[Object] => 
+					{
+						val children = scala.collection.mutable.ListBuffer[Node]()
+						array.foreach ( item => children += buildValueNode(item) )
+						val dataNode = Elem(null, "data", Null, TopScope, children:_*)
+						buildDataTypeNode("array", dataNode)
+					}
+					case map:java.util.Map[String, Object] => 
+					{
+						val members = scala.collection.mutable.ListBuffer[Node]()
+						val iterator = map.entrySet.iterator
+						while (iterator.hasNext) 
+						{
+							val entry = iterator.next
+							members += <member><name>{ entry.getKey }</name>{ buildValueNode(entry.getValue) }</member>
+						}
+						buildDataTypeNode("struct", members)
+					}
 					
 					case _ => throw new Exception("cant or dont know how to cast " + value + " to an xml-rpc parameter")
 				}
 			}
 			
-			def apply(value:String):Parameter = apply("string", value)
-			def apply(value:Int):Parameter = apply("int", value.toString)
-			def apply(value:Double):Parameter = apply("double", value.toString)
-			def apply(value:Boolean):Parameter = apply("boolean", value.toString)
-			// TODO, finish these (float, date, array, map, etc)
+			private def buildDataTypeNode(dataType:String, value:String):Node = buildDataTypeNode(dataType, Text(value))
 			
-			def apply(dataType:String, value:String):Parameter = 
-			{
-				val dataTypeNode = Elem(null, dataType, Null, TopScope, Text(value))
-				val valueNode = Elem(null, "value", Null, TopScope, dataTypeNode)
-				apply(Elem(null, tag, Null, TopScope, valueNode))
-			}
+			private def buildDataTypeNode(dataType:String, values:Seq[Node]):Node = Elem(null, dataType, Null, TopScope, values:_*)
+			
+			private def buildFullNode(dataTypeNode:Node):Node = Elem(null, tag, Null, TopScope, <value>{ dataTypeNode }</value>)
 			
 			def apply(xml:Node) = new Parameter(xml)
+			
+			private def castToSupportedType(value:Any):Object =
+			{
+				value match
+				{
+					case null => null
+					case value:java.lang.String => value
+					case value:java.lang.Integer => value
+					case value:Int => value.asInstanceOf[java.lang.Integer]
+					case value:java.lang.Double => value
+					case value:Double => value.asInstanceOf[java.lang.Double]
+					case value:java.lang.Boolean => value
+					case value:Boolean => value.asInstanceOf[java.lang.Boolean]
+					case value:java.util.Date => value
+					case array:Array[Object] => 
+					{
+						val jarray = new Array[Object](array.length)
+						for (index <- 0 until array.length)
+						{
+							jarray(index) = castToSupportedType(array(index))
+						}
+						jarray
+					}
+					case seq:scala.collection.Seq[Object] => seq.map( item => castToSupportedType(item) ).toArray
+					case map:java.util.Map[String, Object] => 
+					{
+						val jmap = new java.util.LinkedHashMap[String, Object]()
+						val iterator = map.entrySet.iterator
+						while (iterator.hasNext) 
+						{
+							val entry = iterator.next
+							val value = castToSupportedType(entry.getValue)
+							jmap.put(entry.getKey, value)
+						}
+						jmap
+					}
+					case map:scala.collection.Map[String, Object] => 
+					{
+						val jmap = new java.util.LinkedHashMap[String, Object]()
+						map.keySet.foreach ( key =>
+						{
+							val value = castToSupportedType(map(key))
+							jmap.put(key, value)
+						})
+						jmap
+					}
+					// TODO, finish these (base64, extensions, etc)
+					case _ => throw new Exception("cant or dont know how to cast " + value + " to an xml-rpc parameter")
+				}
+			}
+				
+			// XMLRPC ISO8601 format = 19980717T14:08:55
+			private def dateToIso8601(date:java.util.Date):String =
+			{
+				val builder = new StringBuilder(17)
+				builder.append(pad(date.getYear+1900, 4))
+				builder.append(pad(date.getMonth+1))
+				builder.append(pad(date.getDate))
+				builder.append("T")
+				builder.append(pad(date.getHours))
+				builder.append(":")
+				builder.append(pad(date.getMinutes))
+				builder.append(":")
+				builder.append(pad(date.getSeconds))
+				return builder.toString
+			}
+			
+			private def pad(number:Int, size:Int=2):String = String.format("%0" + size + "d", number.asInstanceOf[java.lang.Integer])
+			
 		}
 		
 		class Parameter(xml:Node) extends XmlWrapper(xml)
@@ -58,17 +139,18 @@ package org.simbit.xmpp
 				
 				dataType match
 				{
+					// TODO, finish these (base64, extensions, etc)
 					case "nil" => null
 					case "string" => stringValue
 					case "boolean" => "true" == stringValue.toLowerCase || "yes" == stringValue.toLowerCase || "1" == stringValue
 					case "i4" => stringValue.toInt
 					case "int" =>  stringValue.toInt
 					case "double" => stringValue.toDouble
-					case "dateTime.iso8601" => parseIso8601Date(stringValue)
-					case "base64" => stringValue // TODO: should this be wrapped with a special node?
+					case "dateTime.iso8601" => iso8601ToDate(stringValue)
+					case "base64" => stringValue 
 					case "struct" => 
 					{
-						val map:mutable.Map[String, Any] = new mutable.ListMap[String, Any]
+						val map = new scala.collection.mutable.ListMap[String, Any]
 						(dataNode \ "member").foreach ( node => map += (node \ "name").text -> parseValue((node \ "value")(0)))
 						map
 					}
@@ -78,7 +160,7 @@ package org.simbit.xmpp
 			}
 			
 			// XMLRPC ISO8601 format = 19980717T14:08:55
-			private def parseIso8601Date(value:String):java.util.Date =
+			private def iso8601ToDate(value:String):java.util.Date =
 			{
 				try 
 				{
@@ -89,7 +171,7 @@ package org.simbit.xmpp
 					date.setDate(value.substring(6, 8).toInt)
 					date.setHours(value.substring(9, 11).toInt)
 					date.setMinutes(value.substring(12, 14).toInt)
-					date.setSeconds(value.substring(15).toInt)					
+					date.setSeconds(value.substring(15).toInt)
 					return date
 				} 
 				catch 
